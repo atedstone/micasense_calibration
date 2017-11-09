4a8652e87b786d4b6cc9d7f73f0ab66eb937c515
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-11-09 12:56:29 +0000

    improved script formatting

--- calc_rad2refl.py
+++ calc_rad2refl.py
@@ -6,7 +6,7 @@ end by photos of reflectance panels.
 This script is interactive and plots figure windows. If using over SSH you 
 must enable X-window forwarding.
 
-e.g. calc_rad2refl.py /scratch/rededge_calibration/panel_RP02-1603157-SC.csv /scratch/rededge_calibration/20170723/raw/000/IMG_0000 /scratch/rededge_calibration/20170723/raw/001/IMG_0032 -calmodel /scratch/rededge_calibration/calmodel_sn1620051.config
+e.g. calc_rad2refl.py /scratch/rededge_calibration/panel_RP02-1603157-SC.csv /scratch/rededge_calibration/20170723/raw/000/IMG_0000 /scratch/rededge_calibration/20170723/raw/001/IMG_0032 /scratch/rededge_calibration/20170723/ -calmodel /scratch/rededge_calibration/calmodel_sn1620051.config
 
 Andrew Tedstone (a.j.tedstone@bristol.ac.uk), November 2017
 
@@ -19,12 +19,23 @@ import argparse
 
 import micasense_calibration as mc
 
-parser = argparse.ArgumentParser('Derive radiance-reflectance parameters for a flights bounded at start and end by photos of reflectance panels.')
+parser = argparse.ArgumentParser('Derive radiance-reflectance parameters for \
+ a flights bounded at start and end by photos of reflectance panels.')
 
-parser.add_argument('panel', type=str, help='str, absolute path to CSV file containing calibration factors for your reflectance panel.')
-parser.add_argument('p_pre', type=str, help='str, absolute path to pre-flight set of images of reflectance panel, ending before band number. e.g. /scratch/SET/IMG_0000_')
-parser.add_argument('p_post', type=str, help='str, absolute path to post-flight set of images of reflectance panel, ending before band number. e.g. /scratch/SET/IMG_0000_')
-parser.add_argument('-calmodel', dest='cal_model_fn', default=None, type=str, help='str, absolute path to configuration file containing calibration parameters for camera used in this flight. Only needed when camera firmware < 2.1.0.')
+parser.add_argument('panel', type=str, help='str, absolute path to CSV file \
+	containing calibration factors for your reflectance panel.')
+parser.add_argument('p_pre', type=str, help='str, absolute path to pre-flight \
+ set of images of reflectance panel, ending before band number. \
+ e.g. /scratch/SET/IMG_0000_')
+parser.add_argument('p_post', type=str, help='str, absolute path to \
+	post-flight set of images of reflectance panel, ending before band \
+	number. e.g. /scratch/SET/IMG_0000_')
+parser.add_argument('flight_loc', type=str, help='str, absolute path to \
+	top-level directory of this flight.')
+parser.add_argument('-calmodel', dest='cal_model_fn', default=None, type=str, 
+	help='str, absolute path to configuration file containing calibration \
+	parameters for camera used in this flight. Only needed when camera \
+	firmware < 2.1.0.')
 
 args = parser.parse_args()
 
@@ -63,7 +74,8 @@ for n in range(1, 6):
 mm = []
 cc = []
 for n in range(0, 5):
-	df = pd.DataFrame({'factor':[factors1[n], factors2[n]]}, index=[date1, date2])
+	df = pd.DataFrame({'factor':[factors1[n], factors2[n]]},
+			index=[date1, date2])
 	df['jDate'] = df.index.to_julian_date()
 	X = sm.add_constant(df['jDate'], has_constant='add')
 	model = sm.OLS(df['factor'], X)
@@ -81,6 +93,6 @@ panel_factors['m'] = mm
 panel_factors['c'] = cc
 
 # Save for future use
-fn_out = 'rad2refl_' + date1.strftime('%Y%m%d%-H%M') + '.csv'
+fn_out = args.flight_loc + '/' + 'rad2refl_params.csv'
 panel_factors.to_csv(fn_out)
 print('Parameters saved to %s.' %(fn_out))
