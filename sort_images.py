61e01831d817a3e38d954f0cc1d0da3c1c9ba610
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2018-03-09 16:58:44 +0000

    add script to move images into folder structure needed by photoscan

---
+++ sort_images.py
@@ -0,0 +1,27 @@
+"""
+Use after process_flight_images.py
+Call with path to the flight, e.g. 
+sort_images.py /scratch/process/rededge/20170723/
+"""
+
+import glob
+import subprocess
+import sys
+
+path_in = sys.argv[1]
+
+images = glob.glob(path + '/refl/**/*.tif', recursive=True)
+
+# Rename each image so that folder details are in file name; move to main folder
+for image in images:
+	folders = image.split('/')
+	new_fn = path_in + '/refl/' + folders[-3] + '_' + folders[-2] + '_' + folders[-1]
+	subprocess.call('mv %s %s' %(image, new_fn), shell=True)
+	print(new_fn)
+
+# create one directory per band and move images into them
+for n in range(1,6):
+	subprocess.call('mkdir %s/band%s' %(path, n), shell=True)
+	subprocess.call('mv %s/refl/*_%s.tif band%s/' %(path,n,n), shell=True)
+
+
